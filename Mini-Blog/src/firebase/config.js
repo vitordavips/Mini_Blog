eddb7bbfdc9d6545d86aad91ef07e857a44c0aef
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-09-02 16:20:37 -0300

    integrando o firebase

---
+++ Mini-Blog/src/firebase/config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+import { getfirestore } from "firebase/firebase";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAmz3uDOknGf4Ljp8gCd6RG1FkKNHn2HA",
+  authDomain: "miniblog-35f8d.firebaseapp.com",
+  projectId: "miniblog-35f8d",
+  storageBucket: "miniblog-35f8d.appspot.com",
+  messagingSenderId: "388072132896",
+  appId: "1:388072132896:web:6ad075949a86ff8a674745"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const db = getfirestore(app)
+
+export { db };

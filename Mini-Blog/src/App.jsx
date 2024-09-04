33dabf4591506c4018015dd2a3cc07411f9994b4
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-09-04 16:54:31 -0300

    criando formulário de cadastro

--- Mini-Blog/src/App.jsx
+++ Mini-Blog/src/App.jsx
@@ -5,6 +5,8 @@ import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 // pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
+import Login from './pages/login/login';
+import Register from './pages/register/register';
 
 // components
 import  Navbar  from './components/Navbar';
@@ -22,6 +24,8 @@ function App() {
 
             <Route path='/' element={<Home />}/>
             <Route path='/about' element={<About />}/>
+            <Route path='/login' element={<Login />}/>
+            <Route path='/register' element={<Register />}/>
 
           </Routes>
         </div>

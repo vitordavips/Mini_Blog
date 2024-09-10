92c75c5a54a0250e9768533f03d50fd9568b9cef
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-09-10 17:23:02 -0300

    registrando o usuário no sistema

--- Mini-Blog/src/hooks/useAuthentication.jsx
+++ Mini-Blog/src/hooks/useAuthentication.jsx
@@ -22,5 +22,42 @@ export const useAuthentication = () => {
         if(cancelled){
             return;
         }
+    };
+
+    const createUser = async (data) => {
+        checkIfIsCancelled()
+
+        setLoading(true)
+
+        try{
+            const {user} = await createUserWithEmailAndPassword(
+                auth,
+                data.email,
+                data.password
+            )
+
+            await updateProfile(user, {
+                displayName: data.displayName
+            })
+
+            return user
+
+        } catch(error) {
+            console.log(error.message)
+            console.log(typeof error.message)
+        }
+
+        setLoading(false)
+    };
+    
+    useEffect(() => {
+        return () => setCancelled(true);
+    }, []);
+
+    return {
+        auth,
+        createUser,
+        error,
+        loading,
     }
-}
+};

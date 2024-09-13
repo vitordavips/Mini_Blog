6fb4237ad724804183e63f85ce8b80200871e8cc
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-09-13 16:07:17 -0300

    Login do usuário

--- Mini-Blog/src/hooks/useAuthentication.jsx
+++ Mini-Blog/src/hooks/useAuthentication.jsx
@@ -5,7 +5,8 @@ import {
     createUserWithEmailAndPassword,
     //signInWithEmailAndPassWord,
     updateProfile,
-    signOut
+    signOut,
+    signInWithEmailAndPassword
 } from 'firebase/auth'
 
 import { useState, useEffect } from 'react';
@@ -75,7 +76,34 @@ export const useAuthentication = () => {
     const logout = () => {
         checkIfIsCancelled();
         signOut(auth);
-    }
+    };
+
+    // login - sign in
+    const login = async(data) => {
+        checkIfIsCancelled()
+
+        setLoading(true);
+        setError(false);
+
+        try {
+            await signInWithEmailAndPassword(auth, data.email, data.password);
+            setLoading(false);
+        } catch (error) {
+            let systemErrorMessage;
+
+            if(error.message.includes("user-not-found")){
+                systemErrorMessage = "Usuário não encontrado."
+            } else if(error.message.includes("wrong-password")){
+                systemErrorMessage = "Senha incorreta."
+            } else {
+                systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+            }
+
+            setError(systemErrorMessage);
+            setLoading(false);
+        }
+
+    };
     
     useEffect(() => {
         return () => setCancelled(true);
@@ -87,5 +115,6 @@ export const useAuthentication = () => {
         error,
         loading,
         logout,
+        login,
     }
 };

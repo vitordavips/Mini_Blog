bd9fc37c4bb87db5c95524893becfb9b21dc5254
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-09-24 16:43:36 -0300

    css dos detalhes do post

--- Mini-Blog/src/components/PostDetail.jsx
+++ Mini-Blog/src/components/PostDetail.jsx
@@ -4,11 +4,11 @@ import React from 'react'
 
 export const PostDetail = () => {
   return (
-    <div>
+    <div className={styles.post_detail}>
         <img src={post.image} alt={post.tile} />
         <h2>{post.title}</h2>
-        <p>{post.createdBy}</p>
-        <div>
+        <p className={styles.createdby}>{post.createdBy}</p>
+        <div className={styles.tags}>
             {post.tags.map((tag) => (
                 <p key={tag}>
                     <span>#</span>

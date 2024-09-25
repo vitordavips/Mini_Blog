0d9bb889656bc9d60a1906733e042b6a28218211
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-09-25 17:05:47 -0300

    exibindo resultados da busca

--- Mini-Blog/src/pages/Search/Search.jsx
+++ Mini-Blog/src/pages/Search/Search.jsx
@@ -4,16 +4,37 @@
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
 import { useQuery } from '../../hooks/useQuery';
 
+//components
+import PostDetail from '../../components/PostDetail';
+
+
+import { Link } from 'react-router-dom';
+
+
 const Search = () => {
     const query = useQuery();
     const search = query.get("q");
 
+    const {documents: posts} = useFetchDocuments("posts")
+
   return (
     <div>
         <h2>Search</h2>
-        <p>{search}</p>
+        <div>
+            {posts && posts.length === 0 && (
+                <>
+                    <p>Não foram encontrados posts da sua busca...</p>
+                    <Link to="/" className='btn btn-dark'>
+                        Voltar
+                    </Link>
+                </>
+            )}
+            {posts && posts.map((post) => (
+                <PostDetail key={post.id} post={post} />
+            ))}
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;

1e85fb111a41c6ca03bc229ed4c1b16262c80e64
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-09-26 16:41:35 -0300

    criando página do post individual

--- Mini-Blog/src/App.jsx
+++ Mini-Blog/src/App.jsx
@@ -24,6 +24,7 @@ import { useAuthentication } from './hooks/useAuthentication';
 import CreatePost from './pages/CreatePost/CreatePost';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Search from './pages/Search/Search';
+import Post from './pages/Post/Post';
 
 
 function App() {
@@ -54,10 +55,24 @@ function App() {
               <Route path='/' element={<Home />} />
               <Route path='/about' element={<About />} />
               <Route path='/search' element={<Search />}/>
-              <Route path='/login' element={ !user ? <Login /> : <Navigate to="/" />}  />
-              <Route path='/register' element={!user ? <Register /> : <Navigate to="/" />} />
-              <Route path='/posts/create' element={user ? <CreatePost /> : <Navigate to="/login" />} />
-              <Route path='/dashboard' element={user ? <Dashboard /> : <Navigate to="/login" />} />
+              <Route path='/posts/:id' element={<Post />}/>
+
+              <Route 
+                path='/login' 
+                element={ !user ? <Login /> : <Navigate to="/" />}  
+              />
+              <Route 
+                path='/register' 
+                element={!user ? <Register /> : <Navigate to="/" />} 
+              />
+              <Route 
+                path='/posts/create' 
+                element={user ? <CreatePost /> : <Navigate to="/login" />} 
+              />
+              <Route 
+                path='/dashboard' 
+                element={user ? <Dashboard /> : <Navigate to="/login" />} 
+              />
             </Routes>
           </div>
           <Footer />
---
+++ Mini-Blog/src/pages/Post/Post.jsx
@@ -0,0 +1,14 @@
+import styles from "./Post.module.css";
+
+// hooks
+import { useParams } from "react-router-dom";
+
+const Post = () => {
+    const {id} = useParams();
+
+  return (
+    <div>Post{id}</div>
+  )
+}
+
+export default Post

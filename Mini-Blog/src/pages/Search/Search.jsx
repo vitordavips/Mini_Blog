464670e2073354090307ddbc6f5992d5f4644725
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2024-09-26 16:28:15 -0300

    exibindo resultados da busca
    continuando mexendo

--- Mini-Blog/src/pages/Search/Search.jsx
+++ Mini-Blog/src/pages/Search/Search.jsx
@@ -1,4 +1,4 @@
-//import styles from 'Search.module.css';
+import styles from './Search.module.css';
 
 // hooks
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
@@ -15,19 +15,19 @@ const Search = () => {
     const query = useQuery();
     const search = query.get("q");
 
-    const {documents: posts} = useFetchDocuments("posts")
+    const {documents: posts} = useFetchDocuments("posts", search)
 
   return (
-    <div>
+    <div className={styles.search_container}>
         <h2>Search</h2>
         <div>
             {posts && posts.length === 0 && (
-                <>
+                <div className={styles.noposts}>
                     <p>Não foram encontrados posts da sua busca...</p>
                     <Link to="/" className='btn btn-dark'>
                         Voltar
                     </Link>
-                </>
+                </div>
             )}
             {posts && posts.map((post) => (
                 <PostDetail key={post.id} post={post} />
